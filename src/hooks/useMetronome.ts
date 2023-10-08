4b779748b5c75a12579ea1a1fde3c38562a96a57
nyantama <r_nyantama0616@yahoo.co.jp>
2023-10-08 20:19:42 +0900

    メトロノームに中強拍を追加

--- src/hooks/useMetronome.ts
+++ src/hooks/useMetronome.ts
@@ -3,14 +3,19 @@ import IMetronome from "../interfaces/IMetronome";
 import useSound from "use-sound";
 import tinSound from "./sounds/tin2.mp3";
 import kachiSound from "./sounds/kachi2.mp3";
+import kachiWeakSound from "./sounds/kachi2_weak.mp3";
 
 export default function useMetronome(): IMetronome {
-    const [playTin, { stop: stopTin, pause: pauseTin }] = useSound(tinSound);
-    const [playKachi, { stop, pause }] = useSound(kachiSound);
+    const [playTin, {}] = useSound(tinSound);
+    const [playKachi, {}] = useSound(kachiSound);
+    const [playKachiWeak, {}] = useSound(kachiWeakSound);
     function beat(velocity: number) {
         if (velocity < 50) {
+            playKachiWeak();
+        } else if (velocity < 100) {
             playKachi();
-        } else {
+        }
+        else {
             playTin();
         }
     }
