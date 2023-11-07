96c548d4e65e6f5e767126aad8a98d5a7d12c28c
nyantama <r_nyantama0616@yahoo.co.jp>
2023-11-07 23:36:39 +0900

    MIDIデバイスじゃなくて、KeyPressで操作できるような、useMidiIOのMock作った

--- src/components/pages/TrainingPage.tsx
+++ src/components/pages/TrainingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useMidiIO } from "../../hooks/useMidiIO";
 import useTrainingManager from "../../hooks/useTrainingManager";
 import useTrainRecorder from "../../hooks/useTrainRecorder";
@@ -6,11 +6,21 @@ import TrainMain from "../organisms/TrainMain";
 import TrainConfig from "../organisms/TrainConfig";
 import TrainRecord from "../organisms/TrainRecord";
 import "./TrainingPage.css";
+import useKeyPressManager from "../../hooks/useKeyPressManager";
+import { useMidiIOMock } from "../../hooks/mocks/useMidiIOMock";
 
 export default function TrainingPage() {
-    const midiIO = useMidiIO();
+    const keyPressManager = useKeyPressManager();
+    const midiIO = useMidiIOMock(keyPressManager);
+    // const midiIO = useMidiIO();
     const trainManager = useTrainingManager(midiIO);
     const trainRecorder = useTrainRecorder(trainManager);
+
+    const trainingPageRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        trainingPageRef.current?.focus();
+    }, []);
 
     function _start() {
         trainManager.start();
@@ -25,7 +35,7 @@ export default function TrainingPage() {
     }
 
     return (
-        <div className="train-page">
+        <div className="train-page" tabIndex={0} ref={trainingPageRef} onKeyDown={e => keyPressManager.handleKeyDown(e)} onKeyUp={e => keyPressManager.handleKeyUp(e)}>
             <h1>Training Page</h1>
             <button onClick={_start}>start</button>
             <button onClick={_stop}>stop</button>
---
+++ src/hooks/mocks/useMidiIOMock.ts
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { WebMidi, Input, Output } from "webmidi";
+import Note from "../../enums/Note";
+import IKeyPressManager from "../../interfaces/IKeyPressManager";
+import IMidiIO from "../../interfaces/IMidiIO";
+import { IMidiMessage, MessageType } from "../../interfaces/IMidiMessage";
+
+export function useMidiIOMock(keyPressManager: IKeyPressManager): IMidiIO {
+    const mockMidiDevice = "Mock MIDI Device";
+    const [inputMessage, setInputMessage] = useState<IMidiMessage | null>(null);
+
+    useEffect(() => {
+        switch (keyPressManager.keyDownInfo.key) {
+            case "c":
+                setInputMessage({ type: MessageType.On, note: Note.C4 });
+                break;
+            case "d":
+                setInputMessage({ type: MessageType.On, note: Note.D4 });
+                break;
+            case "b":
+                setInputMessage({ type: MessageType.On, note: Note.B3 });
+                break;
+        }
+    }, [keyPressManager.keyDownInfo]);
+    
+    useEffect(() => {
+        switch (keyPressManager.keyUpInfo.key) {
+            case "c":
+                setInputMessage({ type: MessageType.Off, note: Note.C4 });
+                break;
+            case "d":
+                setInputMessage({ type: MessageType.Off, note: Note.D4 });
+                break;
+            case "b":
+                setInputMessage({ type: MessageType.Off, note: Note.B3 });
+                break;
+        }
+    }, [keyPressManager.keyUpInfo]);
+
+    function setInput(deviceName: string) {
+        
+    }
+
+    function setOutput(deviceName: string) {
+
+    }
+
+    function sendMessage(message: IMidiMessage) {
+        
+    }
+
+    function updateAvailableDevices() {
+
+    }
+
+    return {
+        availableInputDevices: [mockMidiDevice],
+        availableOutputDevices: [mockMidiDevice],
+        currentInputDevice: mockMidiDevice,
+        currentOutputDevice: mockMidiDevice,
+        setInput,
+        setOutput,
+        inputMessage,
+        sendMessage,
+        updateAvailableDevices
+    }
+}
