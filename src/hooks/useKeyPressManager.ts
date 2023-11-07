93dcb1ea0cca43c27c3cd15ab18894c39c166b52
nyantama <r_nyantama0616@yahoo.co.jp>
2023-11-07 19:47:09 +0900

    KeyPressManagerにキーが離された時の処理を追加

---
+++ src/hooks/useKeyPressManager.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+import IKeyPressManager, { IKeyDownInfo, IKeyUpInfo } from "../interfaces/IKeyPressManager";
+
+const initialKeyDownInfo: IKeyDownInfo = {
+    count: 0,
+    key: ""
+}
+
+export default function useKeyPressManager(): IKeyPressManager {
+    const [keyDownInfo, setKeydownInfo] = useState<IKeyDownInfo>(initialKeyDownInfo);
+    const [keyUpInfo, setKeyUpInfo] = useState<IKeyUpInfo>(initialKeyDownInfo);
+
+    useEffect(() => {
+        console.log("init keyPressManager");
+        
+    }, []);
+
+    function handleKeyDown(e: React.KeyboardEvent<Element>) {
+        setKeydownInfo(prev => {
+            const newKeydownInfo = {
+                count: prev.count + 1,
+                key: e.key
+            };
+
+            return newKeydownInfo;
+        });
+    }
+    
+    function handleKeyUp(e: React.KeyboardEvent<Element>) {
+        setKeyUpInfo(prev => {
+            const newKeyUpInfo = {
+                count: prev.count + 1,
+                key: e.key
+            };
+
+            return newKeyUpInfo;
+        });
+    }
+
+    return {
+        keyDownInfo,
+        handleKeyDown,
+        keyUpInfo,
+        handleKeyUp,
+    }
+}
--- src/interfaces/IKeyPressManager.ts
+++ src/interfaces/IKeyPressManager.ts
@@ -2,9 +2,16 @@ import React from "react";
 export default interface IKeyPressManager {
     keyDownInfo: IKeyDownInfo //押されたキーの情報
     handleKeyDown(e: React.KeyboardEvent<Element>): void //この関数をdiv要素などのonKeyDownにセットする
+    keyUpInfo: IKeyUpInfo //離されたキーの情報
+    handleKeyUp(e: React.KeyboardEvent<Element>): void //この関数をdiv要素などのonKeyUpにセットする
 }
 
 export interface IKeyDownInfo {
     count: number //キーが今までに押された回数. 多分countがないと、前回と同じキーを押された時にstateが更新されない
     key: string //どのキーが押されたか
+}
+
+export interface IKeyUpInfo {
+    count: number //キーが今までに離された回数. 多分countがないと、前回と同じキーを押された時にstateが更新されない
+    key: string //どのキーが離されたか
 }
