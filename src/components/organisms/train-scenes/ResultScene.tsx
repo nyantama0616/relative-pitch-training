e6cb7e07c485ef280ed52b191ea59e85feaa59ed
nyantama <r_nyantama0616@yahoo.co.jp>
2023-11-13 19:29:15 +0900

    fetchしてきた配列をグラフとして表示するようにした

--- src/components/organisms/train-scenes/ResultScene.tsx
+++ src/components/organisms/train-scenes/ResultScene.tsx
@@ -1,27 +1,40 @@
 import { Box } from "@mui/system";
+import { useMemo } from "react";
+import useIntervalRatesFetcher from "../../../hooks/useIntervalRatesFetcher";
 import SizingProps from "../../../interfaces/SizingProps";
 import TrainResultGraph from "../TrainResultGraph";
-
+import BasicStatus from "../../../interfaces/BasicStatus";
+import useIntervalRatesFetcherMock from "../../../hooks/mocks/useIntervalRatesFetcherMock";
+import { useEffect } from "react";
 interface ResultSceneProps {
     sizing?: SizingProps;
 }
 
 export default function ResultScene({ sizing }: ResultSceneProps) {
-    const prevMissRates = [0.2, 0.3, 0.4, 0.5, 0.3, 0.1, 0.2, 0.3, 0.4, 1, 0.3, 0.4];
-    const prevAverageReactionRates = [200, 300, 400, 500, 300, 100, 200, 300, 400, 500, 300, 500];
-    const missRates = [0.1, 0.2, 0.3, 0.4, 0.5, 0.3, 0.1, 0.2, 0.3, 0.4, 0.5, 0.3];
-    const averageReactionRates = [100, 200, 300, 400, 600, 300, 100, 200, 300, 400, 500, 300];
+    const prevFetcher = useIntervalRatesFetcherMock();
+    const currFetcher = useIntervalRatesFetcherMock();
 
-    const props = {
-        prevMissRates: prevMissRates,
-        prevAverageReactionRates: prevAverageReactionRates,
-        missRates: missRates,
-        averageReactionRates: averageReactionRates,
-    };
+    const props = useMemo(() => {
+        if (prevFetcher.status === BasicStatus.Success && currFetcher.status === BasicStatus.Success) {
+            return {
+                prevMissRates: prevFetcher.result!.missRates,
+                prevAverageReactionRates: prevFetcher.result!.averageReactionRates,
+                missRates: currFetcher.result!.missRates,
+                averageReactionRates: currFetcher.result!.averageReactionRates,
+            }
+        } else {
+            return null;
+        }
+    }, [prevFetcher.status, currFetcher.status])
+
+    useEffect(() => {
+        prevFetcher.fetch(0);
+        currFetcher.fetch(0);
+    }, []);
 
     return (
         <Box component="div" sx={{ backgroundColor: "#ccccff", ...sizing }}>
-            <TrainResultGraph {...props} />
+            {props ? <TrainResultGraph {...props} /> : null}
         </Box>
     )
 }
