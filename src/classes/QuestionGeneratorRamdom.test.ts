68c5b9e16ee78ed56950dfe3cab247835c239cf1
nyantama <r_nyantama0616@yahoo.co.jp>
2023-11-21 22:32:52 +0900

    QuestionGeneratorRandomクラスを作った
    テストも書いた！

---
+++ src/classes/QuestionGeneratorRamdom.test.ts
@@ -0,0 +1,33 @@
+import QuestionGeneratorRandom from "./QuestionGeneratorRandom";
+import Note from "../enums/Note";
+
+const note1Candidates = [Note.D3, Note.E3, Note.F3, Note.G3, Note.A3, Note.B3, Note.D4, Note.E4, Note.F4, Note.G4, Note.A4, Note.B4];
+
+describe("Test GenerateCounterRandom", () => {
+    test("note0がC4かつnote1がD3~B4であること", () => {
+        const generator = new QuestionGeneratorRandom();
+        const question = generator.generate();
+
+        for (let i = 0; i < 100; i++) {
+            expect(question.note0).toBe(Note.C4);
+            expect(note1Candidates).toContain(question.note1);
+        }
+    });
+    
+    test("ランダムに問題が生成されるか", async () => {
+        const generator = new QuestionGeneratorRandom();
+        
+        let dict: { [key: number]: number } = {};
+        for (let note of note1Candidates) {
+            dict[note] = 0;
+        }
+        
+        for (let i = 0; i < 1000; i++) {
+            const question = generator.generate();
+            dict[question.note1]++;
+        }
+
+        const diffMinMax = Math.max(...Object.values(dict)) - Math.min(...Object.values(dict));
+        expect(diffMinMax).toBeLessThan(50); //TODO: こんな甘々でいいのか？
+    });
+});
---
+++ src/classes/QuestionGeneratorRandom.ts
@@ -0,0 +1,20 @@
+import IQuestionGenerator, { IQuestion } from "../interfaces/IQuestionGenerator";
+import Note from "../enums/Note";
+
+const note1Candidates = [Note.D3, Note.E3, Note.F3, Note.G3, Note.A3, Note.B3, Note.D4, Note.E4, Note.F4, Note.G4, Note.A4, Note.B4];
+
+export default class QuestionGeneratorRandom implements IQuestionGenerator {
+    public generate(): IQuestion {
+        const note0 = Note.C4;
+        const note1 = note1Candidates[this._randInt(0, note1Candidates.length - 1)];
+
+        return {
+            note0, //C4
+            note1 //D3 ~ B3 or D4 ~ B4
+        }
+    }
+    
+    private _randInt(min: number, max: number): number {
+        return min + Math.floor(Math.random() * (max - min + 1));
+    }
+}
