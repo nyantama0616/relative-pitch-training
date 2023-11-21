6aed7b705b500a1f4e242e652a829b289775048c
nyantama <r_nyantama0616@yahoo.co.jp>
2023-11-21 23:31:25 +0900

    App.tsxを綺麗にした
    すべてのページでDIができるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { DependencyProvider } from './contexts/Dependency';
+import Layout from './components/layouts/Layout';
 import TopPage from './components/pages/TopPage';
 import KeyboardTestPage from './components/pages/KeyboardTestPage';
-import "./App.css";
 import SignUpPage from './components/pages/SignUpPage';
 import SignInPage from './components/pages/SignInPage';
-import Layout from './components/layouts/Layout';
 import TrainPage from './components/pages/TrainPage';
-import { DependencyProvider } from './contexts/Dependency';
 import TestRequestManager from './components/pages/TestRequestManager';
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<TopPage/>}></Route>
-          <Route path="/keyboard-test" element={<KeyboardTestPage/>}></Route>
-          <Route path="/train" element={<Layout mainComponent={<TrainPage sizing={{height: "1000px"}}/>} />}></Route>
-          <Route path="/sign-up" element={<Layout mainComponent={<SignUpPage sizing={{height: "1000px"}}/>} />}></Route>
-          <Route path="/sign-in" element={<Layout mainComponent={<SignInPage sizing={{height: "1000px"}}/>} />}></Route>
+          <Route path="/" element={<AppWithDependency mainComponent={<TopPage />}/>}></Route>
+          <Route path="/keyboard-test" element={<AppWithDependency mainComponent={<KeyboardTestPage />}/>}></Route>
+          <Route path="/sign-up" element={<AppWithDependency mainComponent={<SignUpPage sizing={{ height: "1000px" }} />}/>}></Route>
+          <Route path="/sign-in" element={<AppWithDependency mainComponent={<SignInPage sizing={{ height: "1000px" }} />}/>}></Route>
+          <Route path="/train" element={<AppWithDependency mainComponent={<TrainPage sizing={{ height: "1000px" }} />}/>}></Route>
           <Route path="/test/request"element={<AppWithDependency mainComponent={<TestRequestManager/>}/>}></Route>
         </Routes>
       </Router>
