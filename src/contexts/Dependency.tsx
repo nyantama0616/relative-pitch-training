2c5aea464b4db69d65dd56772bc787ed3ef85da0
nyantama <r_nyantama0616@yahoo.co.jp>
2023-11-22 22:38:50 +0900

    DIコンテナ一通り書いた

--- src/contexts/Dependency.tsx
+++ src/contexts/Dependency.tsx
@@ -6,30 +6,54 @@ import IMidiIO from "../interfaces/IMidiIO";
 import IQuestionGenerator from "../interfaces/IQuestionGenerator";
 import useRequestManager from "../hooks/useRequestManager";
 import IRequestManager from "../interfaces/request/IRequestManager";
+import useKeyPressManager from "../hooks/useKeyPressManager";
+import useTrainingManager from "../hooks/useTrainingManager";
+import useTrainRecorder from "../hooks/useTrainRecorder";
+import useMidiIO from "../hooks/useMidiIO";
+import QuestionGeneratorRandom from "../classes/QuestionGeneratorRandom";
+import useTrainRecordSaver from "../hooks/useTrainRecordSaver";
 
 interface DependencyContextType {
-    // keyPressManager: IKeyPressManager;
-    // trainManager: ITrainingManager;
-    // trainRecorder: ITrainRecorder;
-    // midiIO: IMidiIO;
-    // questionGenerator: IQuestionGenerator;
-    
-    // useRequestManager: <Request, Response>() => ReturnType<typeof useRequestManager<Request, Response>>;
+    keyPressManager: IKeyPressManager;
+    trainManager: ITrainingManager;
+    trainRecorder: ITrainRecorder;
+    midiIO: IMidiIO;
+    questionGenerator: IQuestionGenerator;
     useRequestManager: <Request, Response>() => IRequestManager<Request, Response>
 }
 
-const DependencyContext = React.createContext<DependencyContextType | null>(null);
+const initialValue: DependencyContextType = {
+    keyPressManager: null!,
+    trainManager: null!,
+    trainRecorder: null!,
+    midiIO: null!,
+    questionGenerator: null!,
+    useRequestManager: null!,
+};
+
+const DependencyContext = React.createContext<DependencyContextType>(initialValue);
 
 export function useDependency() {
     return useContext(DependencyContext);
 }
 
-
 interface DependencyProviderProps {
     children: React.ReactNode
 }
-export function DependencyProvider({children}: DependencyProviderProps) {
+export function DependencyProvider({ children }: DependencyProviderProps) {
+    const midiIO = useMidiIO();
+    const questionGenerator = new QuestionGeneratorRandom();
+    const keyPressManager = useKeyPressManager();
+    const trainManager = useTrainingManager(midiIO);
+    const trainRecordSaver = useTrainRecordSaver(); //TODO: 消す
+    const trainRecorder = useTrainRecorder(trainManager, trainRecordSaver);
+
     const value: DependencyContextType = {
+        keyPressManager,
+        trainManager,
+        trainRecorder,
+        midiIO,
+        questionGenerator,
         useRequestManager: useRequestManager,
     };
 
